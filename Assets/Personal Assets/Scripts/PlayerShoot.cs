2a9b0665c0868e27e2e858c124ef91af1cdd2be1
Patrick San Juan <psanjuan@sfu.ca>
2017-09-29 18:55:03 -0700

    Player can shoot by opening and closing hand
    - uses a ExtendedFingerDetector on the RigidRoundHand_R to call
    StartShooting and StopShooting methods exposed by the PlayerShoot
    script. PlayerShoot script is attatched the the Player game object.

--- Assets/Personal Assets/Scripts/PlayerShoot.cs
+++ Assets/Personal Assets/Scripts/PlayerShoot.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerShoot : MonoBehaviour {
 
     public GameObject bullet;
+    Coroutine shootRoutine;
+    public float shootDelay;
 	// Use this for initialization
 	void Start () {
 		
@@ -12,10 +14,25 @@ public class PlayerShoot : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-    
-        if (Input.GetKeyDown(KeyCode.Q)) {
-            Instantiate(bullet, HandDirectionGetter.GetPositionOfRightHand(), Quaternion.identity);
-        }
-
+        //if (Input.GetKeyDown(KeyCode.Q)) {
+        //    Instantiate(bullet, HandDirectionGetter.GetPositionOfRightHand(), Quaternion.identity);
+        //}
 	}
+
+    IEnumerator Shooting() {
+        while (true) {
+            Instantiate(bullet, HandDirectionGetter.GetPositionOfRightHand(), Quaternion.identity);
+            yield return new WaitForSeconds(shootDelay);
+        }
+    }
+
+    public void StartShooting() {
+        shootRoutine = StartCoroutine(Shooting());
+    }
+
+    public void StopShooting() {
+        if (shootRoutine != null) {
+            StopCoroutine(shootRoutine);
+        }
+    }
 }
