0e0f3b5c61c039f92f4917be599f0359249f0ab4
Patrick San Juan <psanjuan06.sj@gmail.com>
2017-10-20 17:57:59 -0700

    Fixed any errors caused by rebase

--- Assets/Personal Assets/Scripts/ObjectSpawner.cs
+++ Assets/Personal Assets/Scripts/ObjectSpawner.cs
@@ -3,34 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectSpawner : MonoBehaviour {
-<<<<<<< HEAD
-<<<<<<< InClassWork
-
-=======
->>>>>>> End of Class 5
-=======
->>>>>>> catchUp
     public GameObject obj;
     public int count;
     public float delay;
 
-<<<<<<< HEAD
-<<<<<<< InClassWork
 	// Use this for initialization
 	void Start () {
         StartCoroutine(SpawnObjects());
 	}
-=======
-=======
->>>>>>> catchUp
-    // Use this for initialization
-    void Start() {
-        StartCoroutine(SpawnObjects());
-    }
-<<<<<<< HEAD
->>>>>>> End of Class 5
-=======
->>>>>>> catchUp
 
     IEnumerator SpawnObjects() {
         while (count > 0) {
